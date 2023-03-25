8b5399d61d34bd5f370f76fb63104de94162fb19
Jonathan <Jonathan@DESKTOP-1UB4LN1>
2023-03-25 17:28:45 +1100

    Add project files.

---
+++ IndustryExperienceMain/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IndustryExperienceMain.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
